dbb828c55a1324fe5286a1727960f95ff97aed84
2050802h <2050802h@stu.kobe-u.ac.jp>
2021-10-23 22:11:33 +0900

    section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,19 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+
+  if(( (i + j) ) % 2 == 0){noFill();} // 塗りつぶさない
+   else{ fill(150, 150, 150)} // 灰色
+
+    rect(size * i, size * j, size, size); // 格子
+
+  if(j <= 2 && (i + j) % 2 == 1){ fill(255, 0, 0); }
+   else if(j >= 5 && (i + j) % 2 == 1){fill(0, 0, 0);}
+    else{}
+    // 上3行かつ灰色のところ赤、下3行かつ灰色のところ黒
+
+     ellipse((size * i)+(size / 2), (size * j)+(size / 2), size - 4); //円
+
     }
   }
 }
